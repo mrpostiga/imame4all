79fcb05a76d6d4286b9111af5c817a1174a3ea4f
squid12345678 <squid12345678@googlemail.com>
2011-09-22 17:35:36 +0000

    Tidy up

--- trunk/src/iOS/shared.c
+++ trunk/src/iOS/shared.c
@@ -12,11 +12,8 @@ const char* get_resource_path(char* file)
 
 const char* get_documents_path(char* file)
 {
-  static char documents_path[1024];
-
-//sq  sprintf(documents_path, "/var/mobile/Media/ROMs/iMAME4all/%s", file);
-
+    static char documents_path[1024];
     sprintf(documents_path, IMAMEBASEPATH "/%s", file);
 
-  return documents_path;
+    return documents_path;
 }
--- trunk/src/iOS/shared.h
+++ trunk/src/iOS/shared.h
@@ -11,7 +11,7 @@ extern const char* get_documents_path(char* file);
 #define IMAMEBASEPATH "/var/mobile/Media/ROMs/iMAME4all"
     
 #ifdef MYDEBUG
-#define IMAMEBASEPATH "/var/mobile/Applications/2CC608BB-8BD1-46F6-993C-738EDFE375F6/iMAME4all.app/ROMS"
+#define IMAMEBASEPATH "/var/mobile/Applications/50E5E139-D569-45BC-AE6F-258FB2B69CB3/iMAME4all.app/ROMS"
 #endif
     
 #if defined(__cplusplus)
